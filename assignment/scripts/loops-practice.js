60ae4425b0687dff95d055147530fe4eb6d837ad
Brii SwordLegz <brii.swordlegz@gmail.com>
2024-06-23 12:29:01 -0500

    writing for loops and while loops

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -22,7 +22,7 @@ for (let i=0; i<6; i++) {
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-for (i=3; i<6; i++) {
+for (let i=3; i<6; i++) {
   console.log(i);
 }
 
@@ -31,7 +31,7 @@ for (i=3; i<6; i++) {
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-for (i=2; i<12; i=i+2){
+for (let i=2; i<12; i=i+2){
   console.log(i);
 }
 
@@ -39,7 +39,7 @@ for (i=2; i<12; i=i+2){
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for (i=5; i>-1; i--) {
+for (let i=5; i>-1; i--) {
   console.log(i);
 }
 
@@ -50,9 +50,9 @@ console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
-
-for ( eachStarInTheArray of stars ){
-  console.log('Some stars:', eachStarInTheArray);
+console.log('Some stars:');
+for (const eachStarInTheArray of stars ){
+  console.log(eachStarInTheArray);
 }
 
 //// DONE
@@ -73,8 +73,14 @@ while( index < max){
 
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
-
 console.log('count from 0 to 5');
+let i = 0;
+while(i < 6){
+  console.log(i);
+  i++;
+}
+
+//// DONE
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 
